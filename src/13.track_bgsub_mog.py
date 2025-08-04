6a489ec9d6840032f4134083bcdc44db95f2c031
JUHO <ijuho9555@gmail.com>
2025-08-04 15:35:09 +0900

    영상을 통해 배경제거하기

---
+++ src/13.track_bgsub_mog.py
@@ -0,0 +1,23 @@
+import numpy as np, cv2
+
+#cap = cv2.VideoCapture('../img/walking.avi')
+cap = cv2.VideoCapture(0)
+fps = cap.get(cv2.CAP_PROP_FPS) # 프레임 수 구하기
+delay = int(1000/fps)
+
+# 배경 제거 객체 생성
+fgbg = cv2.createBackgroundSubtractorMOG2(50, 20, detectShadows=False)
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # 배경 제거 마스크 계산
+    fgmask = fgbg.apply(frame)
+    cv2.imshow('frame', frame)
+    cv2.imshow('bgsub', fgmask)
+    if cv2.waitKey(1) & 0xff == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
